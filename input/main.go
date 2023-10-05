03596efabb94520324ee92621d4e20f41c1c6836
Sam Mortenson <samuel@mercury.com>
2023-10-05 11:25:18 -0700

    Revert dumb

--- input/main.go
+++ input/main.go
@@ -18,41 +18,20 @@ func init() {
 	}
 }
 
-var heldKeys map[ebiten.Key]int
-
 type Game struct{}
 
 func (g *Game) Update() error {
-	// init in the map.
-	_, ok := heldKeys[ebiten.KeyArrowLeft]
-	if !ok {
-		heldKeys[ebiten.KeyArrowLeft] = 0
-	}
-
 	if ebiten.IsKeyPressed(ebiten.KeyArrowLeft) {
-		// set in held keys state
-		heldKeys[ebiten.KeyArrowLeft] += 1 // unsafe
-		// pos[0] -= 2
-	} else {
-		// reset state
-		heldKeys[ebiten.KeyArrowLeft] = 0 // delete?
-	}
-	// long press?
-	if heldKeys[ebiten.KeyArrowLeft] > 120 {
-		pos[0] -= 20
-		heldKeys[ebiten.KeyArrowLeft] = 0
-	} else if heldKeys[ebiten.KeyArrowLeft] > 30 {
-		pos[0] -= 2
-		heldKeys[ebiten.KeyArrowLeft] = 0
+		pos[0]--
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyArrowRight) {
-		pos[0] += 2
+		pos[0]++
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyArrowUp) {
-		pos[1] -= 2
+		pos[1]--
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyArrowDown) {
-		pos[1] += 2
+		pos[1]++
 	}
 	return nil
 }
@@ -68,7 +47,6 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 }
 
 func main() {
-	heldKeys = map[ebiten.Key]int{}
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		panic(err)
 	}
